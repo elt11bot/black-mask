952c7e961fbf5af3476c97d73d5a797d43c84375
elt11bot <aryaneskandari1381@gmail.com>
2016-03-09 00:31:45 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -315,7 +315,7 @@ function create_config( )
     "jomlak",
     "admin"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {53406884},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
@@ -595,7 +595,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 0
+our_id = 53406884
 now = os.time()
 math.randomseed(now)
 started = false
